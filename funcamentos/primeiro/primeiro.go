8097cde5754dce88de0bd6a8f83f1c5c5276a4da
João Vitor Gomes Silva <34372105+joaokage1@users.noreply.github.com>
2021-05-06 18:14:26 -0300

    Finished first Go file from Golang course

--- funcamentos/primeiro/primeiro.go
+++ funcamentos/primeiro/primeiro.go
@@ -1,1 +1,18 @@
 package main
+
+import "fmt"
+
+/*
+	Sobre comentários...
+
+	1) Código legível, com comentários que agregam valor
+	2) Evitar comentários óbvios
+	3) Abuse dos comentários no curso
+*/
+
+// A porta de entrada de um programa Go é a função main
+func main() {
+	fmt.Print("Primeiro ")
+	fmt.Println("Programa!")
+	fmt.Print("Curso Go (Golang)")
+}
